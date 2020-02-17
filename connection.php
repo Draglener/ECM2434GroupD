5a0ea960bc20a085c2f4e5f6103b6b676f627847
anneliesetravis <anneliesetravis@gmail.com>
2020-02-17 13:08:59 +0000

    connection file for databases

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "db5000293954.hosting-data.io";
+$username = "dbu477057";
+$password = "2020GroupProject!";
+$database = 'dbs287158';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+else {
+  //echo "Connected successfully <br />";
+}
+?>
