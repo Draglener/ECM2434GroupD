6c9d66d99c52c0d5d4fd716607a2e5e30e1657a1
anneliesetravis <anneliesetravis@gmail.com>
2020-02-17 13:34:09 +0000

    Login form action to connect to 'homepage.php'

---
+++ login.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+require('connection.php');
+
+$username = htmlentities($_POST["username"]) ;
+$tutor = htmlentities($_POST["tutor"]) ;
+
+validate($conn, $tutor, $username);
+
+function validate($conn, $tutor, $username){
+
+
+    $username = $conn->real_escape_string($username);
+  //prepared statements for security
+  $sql = $conn->prepare("SELECT userID, tutorID FROM user WHERE username = ?");
+  $sql->bind_param('s', $username);
+    $sql->execute();
+  $sql->store_result();
+  $sql->bind_result($id, $correctTutor);
+
+  //checking username exists in database
+if ($sql->num_rows > 0) {
+  while ($sql->fetch()) {
+    //checking that passwords match
+      if ($correctTutor === $tutor) {
+        session_start();
+        $_SESSION['id'] = $id;
+          session_start();
+          $_SESSION['status'] = "member";
+          header("Location: map.html");
+        exit();
+      } else {
+      $error = "Incorrect Tutor";
+      $_SESSION["apperror"] = $error ;
+    header("Location: homepage.php");
+      }
+  }
+
+}  else {
+   $error = "Username not found" ;
+$_SESSION["apperror"] = $error ;
+header("Location: homepage.php");
+}
+}
+
+
+$conn->close();
+
+?>
