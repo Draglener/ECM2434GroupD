a14aee4ad59e853cdfa1b49edca1a1e960510c40
anneliesetravis <anneliesetravis@gmail.com>
2020-02-19 16:01:15 +0000

    Fixed error

--- login.php
+++ login.php
@@ -1,32 +1,28 @@
 <?php
-session_start();
 require('connection.php');
 
-$username = htmlentities($_POST["username"]) ;
+$username = htmlentities($_POST["username"]);
 $tutor = htmlentities($_POST["tutor"]) ;
-
 validate($conn, $tutor, $username);
 
 function validate($conn, $tutor, $username){
 
-
     $username = $conn->real_escape_string($username);
+	
   //prepared statements for security
   $sql = $conn->prepare("SELECT userID, tutorID FROM user WHERE username = ?");
   $sql->bind_param('s', $username);
-    $sql->execute();
+  $sql->execute();
   $sql->store_result();
   $sql->bind_result($id, $correctTutor);
 
   //checking username exists in database
 if ($sql->num_rows > 0) {
   while ($sql->fetch()) {
-    //checking that passwords match
-      if ($correctTutor === $tutor) {
-        session_start();
-        $_SESSION['id'] = $id;
+    //checking that tutors match
+      if ($correctTutor == $tutor) {
           session_start();
-          $_SESSION['status'] = "member";
+          $_SESSION['status'] = "student";
           header("Location: map.html");
         exit();
       } else {
@@ -39,7 +35,7 @@ if ($sql->num_rows > 0) {
 }  else {
    $error = "Username not found" ;
 $_SESSION["apperror"] = $error ;
-header("Location: homepage.php");
+//header("Location: homepage.php");
 }
 }
 
