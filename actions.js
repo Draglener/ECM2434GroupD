5bd0eabcd5c303ba75ec8990e882d07de87b2b95
JazsW <60623915+JazsW@users.noreply.github.com>
2020-02-24 17:23:38 +0000

    Create actions.js
    required for the game master main page

---
+++ actions.js
@@ -0,0 +1,32 @@
+function addVis(visName){
+  var x = document.getElementById(visName);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function openPage(evt, cityName) {
+  document.getElementById("AddSection").style.display = "none";
+  document.getElementById("AddSection2").style.display = "none";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function addTutor(){
+
+}
+
+function addRoom(){
+
+}
