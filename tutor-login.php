b7227bf76350291733042f07c05cb03c55e84379
anneliesetravis <anneliesetravis@gmail.com>
2020-02-25 14:55:30 +0000

    Tutor login working

--- tutor-login.php
+++ tutor-login.php
@@ -3,7 +3,7 @@
   session_start();
 
   if ( isset($_SESSION['appuser'])){
-    header('Location: MainScreen.php');
+    header('Location: tutor-main-screen.php');
     $error=null;
 
   } else if( isset($_SESSION['apperror'] )){
@@ -14,74 +14,47 @@
   }
 ?>
 
+
+//need to have a check the data that is submitted against the database
+//correctly generate a session (based on the input?) that is different to the session name for the student login
+
 <html>
   <head>
     <script src="Validate.js"></script>
     <link href="tutor-style.css" rel='stylesheet' type='text/css'/>
     <title>Login</title>
-    <style>
-      body {font-family: Arial, Helvetica, sans-serif; background-color: #98EAFA;}
-
-      input{
-        width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0;
-        display: inline-block;
-        border: 1px solid #ccc;
-        box-sizing: border-box;
-      }
-
-      select{
-          width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0;
-        display: inline-block;
-        border: 1px solid #ccc;
-        box-sizing: border-box;
-      }
 
 
-      button {
-        background-color: black;
-        color: white;
-        padding: 14px 20px;
-        margin: 8px 0;
-        border: none;
-        cursor: pointer;
-        width: 100%;
-      }
 
-      img.peopleExploring{
-        width: 200px;
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-      }
-
-      img.people{
-        width: 200px;
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-      }
-    </style>
   </head>
   <body>
       <div class="loginContent">
         <div class="TableHeader"><h1>Tutor Login</h1></div>
-        <form name="loginForm" action="CheckLogin.php" method="post" onsubmit="return validateForm()">
-        
-        //this needs to be updated with a connection to the database
-        <label for="tutor"><b>Select your tutor id?:</b></label>
-        <select name="tutor" required>
-         <option value='1'>Jonathan Fieldsend</option><option value='2'>Edward Keedwell</option>        </select>
-        <p></p>
+        <form name="t-loginForm" action="tutor-login-action.php" method="post"
+
+
+          <label for="tutor"><b>Tutor:</b></label>
+          <select name="tutor" required id="tutorList">
+           <?php
+              //function to populate drop-down menu from distinct tutorvalues in database
+              function dropdownOptions() {
+                require('connection.php');
+                $sql = "SELECT * FROM tutorGroup";
+                $result = $conn->query($sql);
+                while($row = $result->fetch_assoc()){
+                  echo "<option value='".$row['tutorID']."'>".$row['fName']." ".$row['lName']."</option>";
+                }
+              }
+              dropdownOptions();
+              ?>
+          </select>
 
         <p>Please enter your password:</p>
         <input type="password" name="password" value="">
         <p></p>
-        <input type="submit" value="Login">
+        <button type="submit" name="submit" id="loginButton">Login</button>
         </form>
       </div>
+      <button onclick="window.location.href = 'tutor-main-screen.php';">Click Here</button>
   </body>
 </html>
