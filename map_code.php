24f3169a9cfe4930601d37919fe724ac92dbab92
steven-reyn <sjr239@exeter.ac.uk>
2020-02-25 13:24:05 +0000

    Update map_code.php
    Dev: Steven added map to webpage

--- map_code.php
+++ map_code.php
@@ -27,13 +27,10 @@ Last updated: 12/02 14:00
 -->
 <html>
   <head>
-    <style>
-       /* Set the size of the div element that contains the map */
-      #map {
-        height: 60%;  /* The height is 400 pixels */
-        width: 100%;  /* The width is the width of the web page */
-       }
-    </style>
+    <meta charset="UTF-8">
+		<title>The mapPage</title>
+    <link href="style_sheet.css" rel="stylesheet" type="text/css">
+    
   </head>
   <script>
     var buildings = {}
@@ -49,79 +46,82 @@ Last updated: 12/02 14:00
     
     //To access the name of (e.g) the second building in the cycle, use buildings[2].name
     </script>
-  <body>
-    <h3>Map for Game</h3>
-    <!--The div element for the map -->
-    <div id="map"></div>
-    <button id = "testButton">Add marker</button>
-    <script>
-//Initialize and add the map
-function initMap() {
-
-  var center = { lat: 50.735801, lng:  -3.533297};
-  var innovation = { lat: 50.738045, lng:  -3.530514};
-
-  // The map, centered at the Library
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 16.5,
-    center: center,
-    mapTypeId: 'hybrid',
-    gestureHandling: 'none', 
-    zoomControl:true
-    });
- 
-  var customStyled = [{
-    featureType: "all",
-    elementType: "labels",
-    stylers: [
-      { visibility: "off" }
-    ]
-  }];
-  map.set('styles',customStyled);
-
-  // The markers, positioned at Library
-  
-  for(var i = 0;i<len;i++){
-    var location = {lat: buildings[i].lat,lng: buildings[i].lng};
-    var name = buildings[i].name;
-    var info = buildings[i].info;
-    addMarker(location,map,name,info);
-    console.log(buildings[i].name);
-	console.log("location");
-  }
-  
-  const x = document.getElementById('testButton');
-  var cname = "center"
-  var cinfo = "uwu"
-  x.onclick = addMarker(center,map,cname,cinfo);
-  //x.onclick = function(){var marker = new google.maps.Marker({position: center,map: map,title: 'center'});};
-}
-
-function addMarker(location,map,label,information){
-  var contentString = '<div id="content">'+
-    '<div id="siteNotice">'+
-    '</div>'+
-    '<h1 id="firstHeading" class="firstHeading">'+label+'</h1>'+
-    '<div id="bodyContent">'+
-    '<p>'+information+'</p>'+
-    '</div>'+
-    '</div>';
-
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-  var marker = new google.maps.Marker({position: location,map: map,title: label});
-  marker.addListener('click', function() {
+  <body class="body" id="body">
+    <div>
+		  <a href="FAQ.html"><input type="button" id="homeButton" value="FAQ"></a>
+		  <input type="text" id="pointsDisplayTag" value="&#9733; xxxx points" size="30" maxlength="20">
+    </div>
     
-    infowindow.open(map, marker);
-  });
+    <!--The div element for the map -->
+    <div id="fullMapDisplay"></div>
+    <script>
+    //Initialize and add the map
+    function initMap() {
 
-  closeInfoWindow = function() {
-    infoWindow.close();
-  };
+      var center = { lat: 50.735801, lng:  -3.533297};
+      var innovation = { lat: 50.738045, lng:  -3.530514};
 
-  google.maps.event.addListener(map, 'click', closeInfoWindow);
-}
+      // The map, centered at the Library
+      var map = new google.maps.Map(
+          document.getElementById('fullMapDisplay'), {zoom: 16.5,
+        center: center,
+        mapTypeId: 'hybrid',
+        gestureHandling: 'none', 
+        zoomControl:true
+        });
+    
+      var customStyled = [{
+        featureType: "all",
+        elementType: "labels",
+        stylers: [
+          { visibility: "off" }
+        ]
+      }];
+      map.set('styles',customStyled);
+
+      // The markers, positioned at Library
+      
+      for(var i = 0;i<len;i++){
+        var location = {lat: buildings[i].lat,lng: buildings[i].lng};
+        var name = buildings[i].name;
+        var info = buildings[i].info;
+        addMarker(location,map,name,info);
+        console.log(buildings[i].name);
+      console.log("location");
+      }
+      
+      const x = document.getElementById('testButton');
+      var cname = "center"
+      var cinfo = "uwu"
+      x.onclick = addMarker(center,map,cname,cinfo);
+      //x.onclick = function(){var marker = new google.maps.Marker({position: center,map: map,title: 'center'});};
+    }
+
+    function addMarker(location,map,label,information){
+      var contentString = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">'+label+'</h1>'+
+        '<div id="bodyContent">'+
+        '<p>'+information+'</p>'+
+        '</div>'+
+        '</div>';
+
+      var infowindow = new google.maps.InfoWindow({
+        content: contentString
+      });
+      var marker = new google.maps.Marker({position: location,map: map,title: label});
+      marker.addListener('click', function() {
+        
+        infowindow.open(map, marker);
+      });
+
+      closeInfoWindow = function() {
+        infoWindow.close();
+      };
+
+      google.maps.event.addListener(map, 'click', closeInfoWindow);
+    }
     </script>
     <!--Load the API from the specified URL
     * The async attribute allows the browser to render the page while the API loads
@@ -131,5 +131,10 @@ function addMarker(location,map,label,information){
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCav_BvOFlJ0fMtElRHjkI3xAFPLbe6IY4&callback=initMap">
     </script>
+
+    <div style="margin:10px;">
+		  	<div id="button"><a href="Scoreboard.html"><input type="button" id="ScoreBoardButton" value="&#8682; ScoreBoard"></a></div>
+		    <div id="button"><a href="qr.html"><img type="button" src="qrButton.jpg" alt="QRButton" class="QRButton"></a></div>
+		</div>
   </body>
 </html>
