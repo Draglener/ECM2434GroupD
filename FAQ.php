e3d6122efc104a5d33ef06cc74985c021931c5f8
Draglener <56916474+Draglener@users.noreply.github.com>
2020-03-02 11:32:37 +0000

    Update FAQ.php
    put https:// twice causing page to not be found

--- FAQ.php
+++ FAQ.php
@@ -56,7 +56,7 @@ Adding score to page
 
 		 <div style="margin:10px;">
 		  	<div id="button"><a href="Scoreboard.php"><input type="button" id="ScoreBoardButton" value="&#8682; ScoreBoard"></a></div>
-		    <a data-ga-click="Footer, go to terms, text:terms" href="https://https://www.secondchancelarp.co.uk/ECM2434GroupD-master/ECM2434GroupD-master/terms.php">Terms</a>
+		    <a data-ga-click="Footer, go to terms, text:terms" href="https://www.secondchancelarp.co.uk/ECM2434GroupD-master/ECM2434GroupD-master/terms.php">Terms</a>
 			 <div id="button"><a href="qr.php"><img type="button" src="qrButton.jpg" alt="QRButton" class="QRButton"></a></div>
 		</div>
 <script>
