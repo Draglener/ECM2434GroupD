fd9b80211b19dcf1edd291908dad1bf21f6a93c5
steven-reyn <sjr239@exeter.ac.uk>
2020-03-04 16:05:13 +0000

    Help button
    Updated help button, should work now

--- helpUpdate.php
+++ helpUpdate.php
@@ -5,4 +5,6 @@ $help = $_GET['help'];
 $user = $_SESSION['studentID'];
 $sql2 = $conn->prepare("UPDATE user SET help = '1' WHERE userID = $user");
 $sql2->bind_param('ii', $help, $user);
+
+header("Location: https://www.secondchancelarp.co.uk/ECM2434GroupD-master/ECM2434GroupD-master/map_code.php");
 ?>
