52664f8c3f4d678e5e8256342f594a5b1a48b22f
JazsW <60623915+JazsW@users.noreply.github.com>
2020-03-05 13:44:42 +0000

    score changed to points

--- logout-student.php
+++ logout-student.php
@@ -3,7 +3,7 @@ session_start();
 require('connection.php');
 $ID = $_SESSION['studentID'];
 
-$query = "UPDATE user SET score = 0 AND location = 0 WHERE userID = ".$ID.";";
+$query = "UPDATE user SET points = 0 AND location = 0 WHERE userID = ".$ID.";";
 $conn->query($query);
 // remove all session variables
 session_unset();
