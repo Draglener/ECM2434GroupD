8fd9e4c4778d96469f7738f13e5f5c557553fe23
JazsW <60623915+JazsW@users.noreply.github.com>
2020-03-05 13:30:02 +0000

    Update logout-student.php

--- logout-student.php
+++ logout-student.php
@@ -1,8 +1,14 @@
 <?php
 session_start();
+require(connection.php);
+$ID = $_SESSION['studentID']
+
+$query = "UPDATE user SET score = 0 AND location = 0 WHERE userID = ".$ID.";";
+$conn->query($query);
 // remove all session variables
 session_unset();
 // destroy the session
 session_destroy();
 header('Location: homepage.php');
 ?>
+
