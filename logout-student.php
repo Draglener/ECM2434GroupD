cf942bc0f5befc5008dc5c8268cdb33fb2507d57
Anneliese <anneliesetravis@gmail.com>
2020-03-05 15:17:24 +0000

    reset points and location on logout

--- logout-student.php
+++ logout-student.php
@@ -1,14 +1,21 @@
 <?php
 session_start();
 require('connection.php');
-$ID = $_SESSION['studentID'];
-
-$query = "UPDATE user SET points = 0 AND location = 0 WHERE userID = ".$ID.";";
-$conn->query($query);
-// remove all session variables
-session_unset();
-// destroy the session
-session_destroy();
-header('Location: homepage.php');
+//reset location
+$user = $_SESSION['studentID'];
+         $sql2 = $conn->prepare("UPDATE user SET location = 0, points = 0 WHERE userID = ?");
+		 $sql2->bind_param('i', $user);
+		 if ($sql2->execute()){
+			// remove all session variables
+			session_unset();
+			// destroy the session
+			session_destroy();
+			header('Location: homepage.php');
+			exit();
+			 			  			 
+		 } else {
+			 echo $conn->error;
+		 }
+		 
 ?>
 
