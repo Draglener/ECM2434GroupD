2f2478ba854a9ebdda5290d9ae2619af26205b18
didsomebodysaykatie <56914712+didsomebodysaykatie@users.noreply.github.com>
2020-03-06 11:20:35 +0000

    Update FAQ.php
    More information added

--- FAQ.php
+++ FAQ.php
@@ -48,15 +48,25 @@ Adding score to page
 ">university website</a> has details on the campus GP, disability access, mental wellbeing services, and sexual health clinics.</p>
 					<p><b>Nightline</b></p>
 					<p>NightLine is a student run volunteer service that offers a free phone listening service from 8pm to 8am. You can call them for any reason. Their number can be found on your student card.</p>
+					<p><b>Drop-ins</b></p>
+					<p>The info-point hubs run 10 minute wellbeing drop-ins. This can be helpful to signpost you to other resources, or act as a check in with your wellbeing. Contact your college hub to find out the days and times they take place.</p>
+					
 					<p><h2>Study Areas</h2></p>
 					<p><b>Study Spaces</b></p>
-					There are a number of study spaces across the university. Many buildings have an informal group study space. In addition, in the Forum there is study space in the library, the Santuary, and Devonshire House.</p>
+					<p>There are a number of study spaces across the university. Many buildings have an informal group study space. In addition, in the Forum there is study space in the library, the Santuary, and Devonshire House.</p>
+					<p>During exams, the Forum seminar rooms are also opened as extra study spaces.</p>
+					<p>Study spaces are split into different types based on the allowed level of noise. Quiet study areas must maintain a low level of noise and silent study spaces must remain silent. The Library consists of quiet and silent spaces. Make sure you respect the allowed level of noise in a study space and the other students studying.</p>
+					
 					<p><h2>The Guild</h2></p>
 					<p><b>What is the Guild?</b></p>
-					<p>The Guild is the student union. They represent the interests of the students to the university, facilitate over 250 societies, and offer advice to students on a number of issues. More information on the Guild can be found on <a href="https://www.exeterguild.org/societies/">their website</a>.</p>
+					<p>The Guild is the student union. They represent the interests of the students to the university, facilitate over 250 societies, and offer advice to students on a number of issues. More information on the Guild can be found on <a href="https://www.exeterguild.org/">their website</a>.</p>
 					<p><b>Devonshire House</b></p>
 					<p>Attached to the Guild, Devonshire House contains food outlets, study spaces, and facilities for societies.</p>
 					<p>There are three floors to Devonshire House. DH1 contains an informal study space with sofas, food outlets, and the offices of the Full Time Officers (who run the Guild). DH2 has more places to eat, the joint eXmedia, and Exepose office, and a bank. DH3 contains a silent study space called The Loft.</p>
+					<p><b>Full-Time Officers</b></p>
+					<p>The Guild has Full-Time Officers that are elected each year. They run the Guild and represent the interests of the students to the university. You can find more information on the Full-Time Officers <a href="https://www.exeterguild.org/officers/">here</a>.</p>
+					<p><b>Societies</b></p>
+					<p>The Guild is responsible for over 250 societies. This includes subject societies, music and drama, politics and activism, volunteering, special interest, and the Athletics Union. Societies are run by a committee elected by society members. Information about societies can be found on the <a href="https://www.exeterguild.org/societies/">Guild website</a> where you can also buy membership.</p>>
 					
         </div>
 		</div>
