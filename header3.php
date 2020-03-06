89cee71e1cbe6913ad2fcde2f4403662a53f077e
Draglener <56916474+Draglener@users.noreply.github.com>
2020-03-06 19:00:47 +0000

    Update header3.php
    still allowed link from quiz to map so removed

--- header3.php
+++ header3.php
@@ -3,8 +3,7 @@ echo "<div class='header'>
   <ul class='topnav' id='myNav'>
   <li></li>
   <li></li>
-    <p id='logo'>  <a href='map_code.php'>
-<img class='header_image' src='images/findExeterLogo.png'></a></p>
+  <p id='logo'>  <img class='header_image' src='images/findExeterLogo.png'></p>
   <li></li>
   <li><a href='logout-student.php'>Logout</a></li>
   </ul>
