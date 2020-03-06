8bc69a2a09d5a0da746f12deab4701de60f425a0
Draglener <56916474+Draglener@users.noreply.github.com>
2020-03-06 17:38:20 +0000

    Update cycleUpdate.php

--- cycleUpdate.php
+++ cycleUpdate.php
@@ -3,8 +3,8 @@ session_start();
 require('connection.php');
 $cycle = $_GET['cycle'];
 $user = $_SESSION['studentID'];
-$sql2 = $conn->prepare("UPDATE user SET currentCycle = $cycle WHERE userID = $user");
-$sql2->bind_param('ii', $currentCycle, $user);
+$sql2 = $conn->prepare("UPDATE user SET currentCycle = ? WHERE userID = ?");
+$sql2->bind_param('ii', $cycle, $user);
 $sql2->execute();
 header("Location: quiz.php");
 ?>
