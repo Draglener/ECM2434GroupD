944f2385de0e20f1e9ab05951346c329157d3d4a
Draglener <56916474+Draglener@users.noreply.github.com>
2020-03-06 10:27:04 +0000

    Update logout-student.php
    Changed it so only reset location and not score

--- logout-student.php
+++ logout-student.php
@@ -3,7 +3,7 @@ session_start();
 require('connection.php');
 //reset location
 $user = $_SESSION['studentID'];
-         $sql2 = $conn->prepare("UPDATE user SET location = 0, points = 0 WHERE userID = ?");
+         $sql2 = $conn->prepare("UPDATE user SET location = 0 WHERE userID = ?");
 		 $sql2->bind_param('i', $user);
 		 if ($sql2->execute()){
 			// remove all session variables
