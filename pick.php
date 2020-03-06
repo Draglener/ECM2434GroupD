d725ccebffaa3ffadb2dc5fe1fb1d2b988c559ce
Draglener <56916474+Draglener@users.noreply.github.com>
2020-03-06 17:37:35 +0000

    Update pick.php

--- pick.php
+++ pick.php
@@ -1,5 +1,21 @@
 <?php
   session_start();
+
+require('connection.php');
+$session = $_SESSION['appuser'];
+if ($_SESSION['status'] == "student"){
+}else{
+  header('Location: homepage.php');
+}
+$sql = "SELECT * from user WHERE userID = ".$_SESSION['studentID'];
+$result =  $conn->query($sql);
+if ($result->num_rows > 0) {
+  while ($row = $result->fetch_assoc()){
+	  $currentCycle = $row['currentCycle'];
+  }
+}
+
+
   ?>
   <!-- Author: Keith Harrison
 Last updated: 06/02 15:54
@@ -16,9 +32,12 @@ Created pick cycle page
   </head>
 
   <body class="body" id="body">
+	<script>
+	var currentCycl = <?php echo $currentCycle;?>;
+	</script>
 
 
-        <img src="findExeterLogo.png" alt="topImage" class="topImage">
+        <img src="images/findExeterLogo.png" alt="topImage" class="topImage">
 
  <form  name="loginForm" action="cycleUpdate.php" method="post">
         <p class="error"><?php echo $_SESSION['apperror']; ?></p>
@@ -45,7 +64,7 @@ Created pick cycle page
 
 
 
-        <img src="exeterLogo.png" alt="bottomImage" class="bottomImage">
+        <img src="images/exeterLogo.png" alt="bottomImage" class="bottomImage">
 
   </body>
   <?php unset($_SESSION['apperror']); ?>
