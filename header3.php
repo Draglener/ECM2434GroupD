4e195cd3ae2aa443a20f55b93470dc7097f435a5
Draglener <56916474+Draglener@users.noreply.github.com>
2020-03-06 18:43:57 +0000

    Create header3.php
    Header for the quiz page

---
+++ header3.php
@@ -0,0 +1,12 @@
+<?php
+echo "<div class='header'>
+  <ul class='topnav' id='myNav'>
+  <li></li>
+  <li></li>
+    <p id='logo'>  <a href='map_code.php'>
+<img class='header_image' src='images/findExeterLogo.png'></a></p>
+  <li></li>
+  <li><a href='logout-student.php'>Logout</a></li>
+  </ul>
+  </div>";
+?>
