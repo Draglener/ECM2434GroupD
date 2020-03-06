8c2a0d256ca28694a9d6d80c3d6a96246aaceb1e
Draglener <56916474+Draglener@users.noreply.github.com>
2020-03-06 18:28:58 +0000

    Update header.php
    Link from clicking the logo to map

--- header.php
+++ header.php
@@ -3,7 +3,8 @@ echo "<div class='header'>
   <ul class='topnav' id='myNav'>
   <li><a href='FAQ.php'>FAQ</a></li>
   <li><a href='scoreboard.php'>Scoreboard</a></li>
-  <p id='logo'>  <img class='header_image' src='images/findExeterLogo.png'></p>
+  <p id='logo'>  <a href='map_code.php'>
+<img class='header_image' src='images/findExeterLogo.png'></a></p>
   <li><a href='qr.php'>Scan QR</a></li>
   <li><a href='logout-student.php'>Logout</a></li>
   </ul>
