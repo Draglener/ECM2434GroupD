3048aee2a73ac0149cf7e6ebe316af38004b2a7a
Draglener <56916474+Draglener@users.noreply.github.com>
2020-03-06 15:35:55 +0000

    Create header2.php
    new header for terms page

---
+++ header2.php
@@ -0,0 +1,11 @@
+<?php
+echo "<div class='header'>
+  <ul class='topnav' id='myNav'>
+  <li><a href='homepage.php'>Back To Login</a></li>
+  <li></li>
+  <p id='logo'>  <img class='header_image' src='images/findExeterLogo.png'></p>
+  <li></li>
+  <li></li>
+  </ul>
+  </div>";
+?>
