e9b1c8b2c278ec8f34f4862f1e40662186478b37
Draglener <56916474+Draglener@users.noreply.github.com>
2020-03-07 22:08:39 +0000

    Update logout-student.php

--- logout-student.php
+++ logout-student.php
@@ -5,7 +5,7 @@ require('connection.php');
 $from = htmlentities($_GET["from"]);
 $user = $_SESSION['studentID'];
 	if ($from == 1){
-         $sql2 = $conn->prepare("UPDATE user SET location = 0 WHERE userID = ?");
+         $sql2 = $conn->prepare("UPDATE user SET location = 0, quizDone = 0 WHERE userID = ?");
 		 $sql2->bind_param('i', $user);
 		 if ($sql2->execute()){
 			// remove all session variables
