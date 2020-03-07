29a3fd977f4a1b9080feb12c4c2e8e44982a3b7c
Draglener <56916474+Draglener@users.noreply.github.com>
2020-03-07 20:57:32 +0000

    Update qr.php
    updated qr to set quizdone to 0 again allowing the quiz to be taken

--- qr.php
+++ qr.php
@@ -43,6 +43,15 @@ if ($result->num_rows > 0) {
 } else {
   echo $sql." ".$conn->error;
 }
+
+$sql = "SELECT * from user WHERE userID = ".$_SESSION['studentID'];
+$result =  $conn->query($sql);
+if ($result->num_rows > 0) {
+  while ($row = $result->fetch_assoc()){
+    $currentQuizDone = $row['quizDone'];
+  }
+}
+
 ?>
 
 <!-- Author: Piranavie Thangasuthan and Katie Jones and Keith Harrison
@@ -68,9 +77,10 @@ Added
     var len = 0;
     for (i = 0; i < n; i++) {
     buildings[i] = {id: prep[i][0], name: prep[i][1], info:prep[i][2],  lat:prep[i][3], lng:prep[i][4]};
-    len++;
-	  
+    len++;	  
     }
+	var currentquizDone = <?php echo $currentQuizDone; ?>;
+
 
     function onQRCodeScanned(scannedText)
     {
@@ -79,7 +89,8 @@ Added
 		   window.location.href = "scoreboard.php"}
 		   
 		else{
-		window.location.href = "quiz.php"}
+		
+		window.location.href = "quizUpdate.php?from=currentquizDone"}
 
 		
 	}	
