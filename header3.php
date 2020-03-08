27beaf91883e66a42cf83bdcfa96c219ddb15a26
Anneliese <anneliesetravis@gmail.com>
2020-03-08 13:50:27 +0000

    logout dropdown

--- header3.php
+++ header3.php
@@ -5,49 +5,33 @@ echo "<div class='header'>
   <li></li>
   <p id='logo'>  <img class='header_image' src='images/findExeterLogo.png'></p>
   <li></li>
-  <li><a id='myBtn'>Logout</a></li>
+   <div class='dropdown'>
+  <li onclick='toggle()' class='dropbtn'>Logout</li>
+ <div id='drop' onclick='toggle()' class='dropdown-content'>
+ <li><a href='logout-student.php?from=1'>Reset Location</a></li>
+ <li><a href='logout-student.php?from=2'>Save Location</a></li>
+ </div>
+  </div>
   </ul>
   </div>";
-echo '
-<div id="myModal" class="modal">
-  <div class="modal-content">
-    <div class="modal-header">
-      <span class="close">&times;</span>
-      <h2>Logout</h2>
-    </div>
-    <div class="modal-body">
-      <p>Would you like to reset your location?</p>
-      <p id = yes>Yes</p>
-      <p id = no>No</p>
-    </div>
-  </div>
-</div>';
-echo '<script type = "text/javascript">
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
-var yes = document.getElementById("yes");
-var no = document.getElementById("no");
-
-
-btn.onclick = function() {
-  modal.style.display = "block";
+  
+  echo "<script>
+  function toggle() {
+  var x = document.getElementById('drop');
+  var y = document.getElementsByClassName('container');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+	y.item(0).style.zIndex='-1';
+	x.style.zIndex='1';
+  } else {
+    x.style.display = 'none';
+	y.item(0).style.zIndex='1';
+	x.style.zIndex='-1';
+  }
 }
 
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-yes.onclick = function(){
-    location.replace("logout-student.php?from=1");
-}
-
-no.onclick = function(){
-   location.replace("logout-student.php?from=2");
-    
-}
-</script>';
-
+  </script>";
+  
 
 ?>
 
