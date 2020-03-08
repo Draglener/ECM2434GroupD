aa4915ef6dd7074db8ee5dd989bf9c24ff109228
Draglener <56916474+Draglener@users.noreply.github.com>
2020-03-08 15:53:39 +0000

    Update header2.php

--- header2.php
+++ header2.php
@@ -1,7 +1,7 @@
 <?php
 echo "<div class='header'>
   <ul class='topnav' id='myNav'>
-  <li><a href='homepage.php'>Login Page</a></li>
+  <li><a href='homepage.php'>Login</a></li>
   <li></li>
   <p id='logo'>  <img class='header_image' src='images/findExeterLogo.png'></p>
   <li></li>
