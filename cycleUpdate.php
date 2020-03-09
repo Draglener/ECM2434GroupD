76cbf82be628264afe1b0dc2252292ae702d53d0
steven-reyn <48689315+steven-reyn@users.noreply.github.com>
2020-03-09 16:23:34 +0000

    Added comments

--- cycleUpdate.php
+++ cycleUpdate.php
@@ -1,8 +1,14 @@
+<!-- 
+Author: Keith Harrison 
+Last updated: 09/03 13:24
+-->
+
 <?php
 Session_start();
 Require('connection.php');
 $cycle = htmlentities($_POST["cycle"]);
 $user = $_SESSION['studentID'];
+//Changes the user's cycle
 $sql2 = "UPDATE user SET currentCycle = ".$cycle." WHERE userID = ".$user;
 $conn->query($sql2);
 header("Location: pick.php");
