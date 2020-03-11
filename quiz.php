0f962d5f0f3d6ca9703e290fcaa2978634bca8b0
Draglener <56916474+Draglener@users.noreply.github.com>
2020-03-11 18:20:45 +0000

    Update quiz.php
    added favicon

--- quiz.php
+++ quiz.php
@@ -111,9 +111,11 @@ if ($result->num_rows > 0) {
 <html>
   <head>
     <meta charset="UTF-8">
+    <link rel="shortcut icon" type="image/png" href="images/findExeterLogo.png"/>
+    <img src="images/findExeterLogo.png" height="150px" style="float: right;">
     <title>Quiz</title>
     <link href="style_sheet.css" rel="stylesheet" type="text/css">
-	  	<link rel="shortcut icon" type="image/png" href="findExeterLogo.png"/>
+
 	<style>
 	#quiz{
 		display:grid;
