67ed5559585ad19139d866b18e788e528952863c
steven-reyn <48689315+steven-reyn@users.noreply.github.com>
2020-03-11 13:30:23 +0000

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,3 @@
-<!-- 
-Author: Jasmine West and Keith Harrison 
-Last updated: 09/03 13:24
--->
 <?php
 session_start();
 // remove all session variables
@@ -10,3 +6,8 @@ session_unset();
 session_destroy();
 header('Location: index.php');
 ?>
+
+<!-- 
+Author: Jasmine West and Keith Harrison 
+Last updated: 09/03 13:24
+-->
